370219ecb0be523be3b8b9909d5b31e4b43adb8b
Oleksandr Veshcherovych <s.veshcherovych@gmail.com>
2018-04-20 01:57:21 +0300

    show user recomendations

--- app/jobs/bayes_classifier_job.rb
+++ app/jobs/bayes_classifier_job.rb
@@ -7,12 +7,22 @@ class BayesClassifierJob < ApplicationJob
 
   def self.likehood(user, video)
     bayes_classifier = NBayes::Base.new
-    user.user_videos.liked.each do |v|1
+    user.user_videos.liked.each do |v|
       bayes_classifier.train v.video.tags.split(' '), 'like'
     end
     user.user_videos.disliked.each do |v|
       bayes_classifier.train v.video.tags.split(' '), 'dislike'
     end
-    puts bayes_classifier.classify(video.tags.split(' '))
+    bayes_classifier.classify(video.tags.split(' '))
   end
+
+  def self.show_recommended_videos(user)
+    results = []
+    Video.all.each do |v|
+      likehood = BayesClassifierJob.likehood(user, v)['like']
+      results << { likehood: likehood, video_id: v.id }
+    end
+    puts results.sort_by { |hsh| hsh[:likehood]  }
+  end
+
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -2,6 +2,8 @@ class Video < ApplicationRecord
   has_many :user_videos
   has_many :users, through: :user_videos
 
+  # scope :seen_by_user, -> (user) { joins(:user_videos).where(user: user) }
+
   # scope :liked, -> () { where(like: true) }
   # scope :disliked, -> () { where(like: false) }
 end
